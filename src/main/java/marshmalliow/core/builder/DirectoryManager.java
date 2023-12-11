594549bfabdc36a03e5c254c92e738e2817774d8
278deco <278deco.officiel@gmail.com>
2023-12-11 19:48:06 +0100

    Updated DirectoryManager to use a ReadWriteLock

--- src/main/java/marshmalliow/core/builder/DirectoryManager.java
+++ src/main/java/marshmalliow/core/builder/DirectoryManager.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,7 +16,7 @@ public class DirectoryManager {
 	
 	private static final Logger LOGGER = LogManager.getLogger(DirectoryManager.class);
 	
-	private final Object mutex = new Object();
+	private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
 	private Set<Directory> directories = new HashSet<>();
 	
@@ -23,26 +24,28 @@ public class DirectoryManager {
 	}
 	
 	public boolean registerNewDirectory(Directory dir) {
-		synchronized (mutex) {
-			try {
-				if(this.directories.add(dir)) {
-					if(Files.createDirectories(dir.getPath()) != null) LOGGER.info("Directory "+dir.getName()+" has been successfully created!");
-					else LOGGER.info("Directory "+dir.getName()+" has been successfully loaded!");
-					
-					return true;
-				}
-				return false;
-			}catch(SecurityException | IOException e) {
-				LOGGER.error("An error occured while creating directory",dir.getName(),". Skipping 1 directory.",e.getMessage());
-				return false;
+		try {
+			lock.writeLock().lock();
+			
+			if(this.directories.add(dir)) {
+				if(Files.createDirectories(dir.getPath()) != null) LOGGER.info("Directory "+dir.getName()+" has been successfully created!");
+				else LOGGER.info("Directory "+dir.getName()+" has been successfully loaded!");
+				
+				return true;
 			}
+			return false;
+		}catch(SecurityException | IOException e) {
+			LOGGER.error("An error occured while creating directory",dir.getName(),". Skipping 1 directory.",e.getMessage());
+			return false;
+		}finally {
+			lock.writeLock().unlock();
 		}
 	}
 	
 	public boolean registerNewDirectoryIfAbsent(Directory dir) {
-		synchronized (mutex) {
 			if(!this.directories.contains(dir)) {
 				try {
+					lock.writeLock().lock();
 					if(this.directories.add(dir)) {
 						if(Files.createDirectories(dir.getPath()) != null) LOGGER.info("Directory "+dir.getName()+" has been successfully created!");
 						else LOGGER.info("Directory "+dir.getName()+" has been successfully loaded!");
@@ -53,14 +56,17 @@ public class DirectoryManager {
 				}catch(SecurityException | IOException e) {
 					LOGGER.error("An error occured while creating directory",dir.getName(),". Skipping 1 directory.",e.getMessage());
 					return false;
+				}finally {
+					lock.writeLock().unlock();
 				}
 			}
 			return false;
-		}
+		
 	}
 	
 	public void registerNewDirectory(Directory... directories) {
-		synchronized (mutex) {
+		try {
+			lock.writeLock().lock();
 			for (Directory dir : directories) {
 				try {
 					if(this.directories.add(dir)) {
@@ -71,35 +77,50 @@ public class DirectoryManager {
 					LOGGER.error("An error occured while creating directory",dir.getName(),". Skipping 1 directory.",e.getMessage());
 				}
 			}
+		}finally {
+			lock.writeLock().unlock();
 		}
+		
 	}
 	
 	public boolean removeDirectory(Directory dir) {
-		synchronized (mutex) {
+		try {
+			lock.writeLock().lock();
 			if(this.directories.remove(dir)) {
 				LOGGER.info("Directory "+dir.getName()+" has been successfully unloaded!");
 				return true;
 			}
 			
 			return false;
+		}finally {
+			lock.writeLock().unlock();
 		}
 	}
 
 	public Directory getLoadedDirectory(String nameId) {
-		synchronized (mutex) {
+		try {
+			lock.readLock().lock();
 			return this.directories.parallelStream().filter(dir -> dir.isNameSimilar(nameId)).findFirst().orElse(null);
+		}finally {
+			lock.readLock().unlock();
 		}
 	}
 	
 	public Directory getLoadedDirectory(Path path) {
-		synchronized (mutex) {
+		try {
+			lock.readLock().lock();
 			return this.directories.parallelStream().filter(dir -> dir.isPathSimilar(path)).findFirst().orElse(null);
+		}finally {
+			lock.readLock().unlock();
 		}
 	}
 	
 	public boolean exist(Directory dir) {
-		synchronized (mutex) {
+		try {
+			lock.readLock().lock();
 			return this.directories.contains(dir);
+		}finally {
+			lock.readLock().unlock();
 		}
 	}
 	
@@ -108,6 +129,11 @@ public class DirectoryManager {
 	 * @return the size of the directories's list
 	 */
 	public int getDirectoriesNumber() {
-		return this.directories.size();
+		try {
+			lock.readLock().lock();
+			return this.directories.size();
+		}finally {
+			lock.readLock().unlock();
+		}
 	}
 }
