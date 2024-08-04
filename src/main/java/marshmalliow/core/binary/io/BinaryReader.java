f432bdf58f9c5b8b9834c740882c087576ccae34
278deco <278deco.officiel@gmail.com>
2024-08-04 16:51:34 +0200

    doc(BinaryIO): Added docs for BinaryWriter and Reader

--- src/main/java/marshmalliow/core/binary/io/BinaryReader.java
+++ src/main/java/marshmalliow/core/binary/io/BinaryReader.java
@@ -8,10 +8,16 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.Objects;
 
+import marshmalliow.core.binary.MOBFFile;
 import marshmalliow.core.binary.utils.Charset;
 import marshmalliow.core.exceptions.DatetimeDataFormatException;
 import marshmalliow.core.objects.DateTime;
 
+/**
+ * A class for reading binary data from a stream, designed for {@link MOBFFile} files.
+ * @version 1.0.0
+ * @author 278deco
+ */
 public class BinaryReader {
 
 	private static final int BUFFER_MIN_SIZE = 64;
@@ -34,6 +40,12 @@ public class BinaryReader {
 		this.in = in;
 	}
 
+	/**
+	 * Closes the stream and releases any system resources associated with it.<br/>
+	 * Once the stream has been closed, further read() invocations will throw an IOException.<br/>
+	 * Multiple calls to close are allowed and will have no effect.
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		if(closed) return;
 
@@ -50,20 +62,47 @@ public class BinaryReader {
 		}
 	}
 	
+	/**
+	 * Read up to <em>len</em> bytes from the stream. 
+	 * @param b the buffer into which the data is read.
+	 * @return the total number of bytes read into the buffer, or -1 if there is no more data because the end of the stream has been reached.
+	 * @see InputStream#read(byte[], int, int)
+	 * @throws IOException
+	 */
 	public int read(byte[] b) throws IOException {
 		return in.read(b, 0, b.length);
 	}
 
+	/**
+     * Read up to <em>length</em> bytes from the stream. 
+     * @param b the buffer into which the data is read.
+     * @param offset the start offset in the buffer at which the data is written.
+     * @param length the maximum number of bytes to read.
+     * @return the total number of bytes read into the buffer, or -1 if there is no more data because the end of the stream has been reached.
+     * @see InputStream#read(byte[], int, int)
+     * @throws IOException
+     */
 	public int read(byte[] b, int offset, int length) throws IOException {
 		return in.read(b, offset, length);
 	}
 
 
+	/**
+	 * Read up to <em>len</em> bytes from the stream. This method read until the array is full or the end of the stream is reached.
+	 * @param b the buffer into which the data is read.
+	 * @throws IOException
+	 */
 	public void readFully(byte[] b) throws IOException {
 		this.readFully(b, 0, b.length);
 	}
 
-
+	/**
+	 * Read up to <em>len</em> bytes from the stream. This method read until the array is full or the end of the stream is reached.
+	 * @param b the buffer into which the data is read.
+	 * @param off the start offset in the buffer at which the data is written
+	 * @param len the maximum number of bytes to read
+	 * @throws IOException
+	 */
 	public void readFully(byte[] b, int off, int len) throws IOException {
 		Objects.checkFromIndexSize(off, len, b.length);
         int n = 0;
@@ -76,6 +115,12 @@ public class BinaryReader {
 	}
 
 
+	/**
+	 * Skips over and discards <em>n</em> bytes of data from the input stream.
+	 * @param n the number of bytes to be skipped.
+	 * @return the actual number of bytes skipped.
+	 * @throws IOException
+	 */
 	public int skipBytes(int n) throws IOException {
 		int skipped = 0;
 		int curr = 0;
@@ -88,6 +133,13 @@ public class BinaryReader {
 	}
 
 
+	/**
+	 * Reads a boolean from the input
+	 * 
+	 * @return the byte value converted to a boolean
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException 
+	 */
 	public boolean readBoolean() throws IOException {
 		int bl = in.read();
 		if(bl < 0) throw new EOFException();
@@ -95,7 +147,13 @@ public class BinaryReader {
 		return (bl != 0);
 	}
 
-
+	/**
+	 * Reads a byte from the input
+	 * 
+	 * @return the byte value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public byte readByte() throws IOException {
 		int b = in.read();
 		if(b < 0) throw new EOFException();
@@ -103,7 +161,13 @@ public class BinaryReader {
 		return (byte)b;
 	}
 
-
+	/**
+	 * Reads an unsigned byte from the input. Because Java doesn't have unsigned byte, it is returned as an integer.
+	 * 
+	 * @return the unsigned byte value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public int readUnsignedByte() throws IOException {
 		int ubyte = in.read();
 		if(ubyte < 0) throw new EOFException();
@@ -111,7 +175,13 @@ public class BinaryReader {
 		return ubyte;
 	}
 
-
+	/**
+	 * Reads a short from the input. The short is read as two bytes, with the first byte being the most significant.
+	 * 
+	 * @return the short value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public short readShort() throws IOException {
 		int short1 = in.read();
 		int short2 = in.read();
@@ -120,7 +190,14 @@ public class BinaryReader {
 		return (short)((short1 << 8) + short2);
 	}
 
-
+	/**
+	 * Reads an unsigned short from the input. The short is read as two bytes, with
+	 * the first byte being the most significant. Because Java doesn't have unsigned short, it is returned as an integer.
+	 * 
+	 * @return the unsigned short value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public int readUnsignedShort() throws IOException {
 		int short1 = in.read();
 		int short2 = in.read();
@@ -129,19 +206,40 @@ public class BinaryReader {
 		return ((short1 << 8) + short2);
 	}
 
-
+	/**
+	 * Reads a char from the input. The char is read as two bytes, with the first
+	 * byte being the most significant.
+	 * 
+	 * @return the char value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public char readChar() throws IOException {
 		return (char)readUnsignedShort();
 	}
 
-
+	/**
+	 * Reads an integer from the input. The integer is read as four bytes, with the
+	 * first byte being the most significant.
+	 * 
+	 * @return the integer value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public int readInt() throws IOException {
 		readFully(readBuffer, 0, 4);
 		
 		return (int) (((readBuffer[0] & 0xFF) << 24) + ((readBuffer[1] & 0xFF) << 16) + ((readBuffer[2] & 0xFF) << 8) + (readBuffer[3] & 0xFF));
 	}
 
-
+	/**
+	 * Reads a long from the input. The long is read as eight bytes, with the first
+	 * byte being the most significant.
+	 * 
+	 * @return the long value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public long readLong() throws IOException {
 		readFully(readBuffer, 0, 8);
 
@@ -151,17 +249,40 @@ public class BinaryReader {
 				+ ((readBuffer[6] & 255) << 8) + (readBuffer[7] & 255);
 	}
 
-
+	/**
+	 * Reads a float from the input. The float is read as four bytes, with the first
+	 * byte being the most significant. We use {@link Float#intBitsToFloat(int)} to convert the integer to a float.
+	 * 
+	 * @return the float value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public float readFloat() throws IOException {
 		return Float.intBitsToFloat(readInt());
 	}
 
-
+	/**
+	 * Reads a double from the input. The double is read as eight bytes, with the
+	 * first byte being the most significant. We use
+	 * {@link Double#longBitsToDouble(long)} to convert the long to a double.
+	 * 
+	 * @return the double value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public double readDouble() throws IOException {
 		return Double.longBitsToDouble(readLong());
 	}
 
 
+	/**
+	 * Reads a string from the input. The string is read depending on the {@link Charset}.<br/>
+	 * <strong>For now only UTF-8 is supported</strong>
+	 * 
+	 * @return the string value
+	 * @throws EOFException if the end of the stream is reached
+	 * @throws IOException
+	 */
 	public String readUTF(Charset charset) throws IOException {
 		switch(charset) {
 			case UTF16:
@@ -245,6 +366,14 @@ public class BinaryReader {
 		return new String(charBuffer, 0, charBufferIndex);
 	}
 
+	/**
+     * Reads a {@link DateTime} from the input. The DateTime is read depending on the control byte.
+     * 
+     * @return the DateTime value
+     * @throws EOFException if the end of the stream is reached
+     * @throws IOException
+     * @throws DatetimeDataFormatException if the control byte is 0x00
+     */
 	public DateTime readDatetime() throws IOException {
 		final DateTime.Builder builder = DateTime.builder();
 		final int controlByte = readByte(); //Read the control byte
--- src/main/java/marshmalliow/core/binary/io/BinaryWriter.java
+++ src/main/java/marshmalliow/core/binary/io/BinaryWriter.java
@@ -5,9 +5,15 @@ import java.io.OutputStream;
 import java.io.UTFDataFormatException;
 import java.time.ZoneOffset;
 
+import marshmalliow.core.binary.MOBFFile;
 import marshmalliow.core.binary.utils.Charset;
 import marshmalliow.core.objects.DateTime;
 
+/**
+ * A class for writing binary data to a stream, designed for {@link MOBFFile} files.
+ * @version 1.0.0
+ * @author 278deco
+ */
 public class BinaryWriter {
 
 	private static final int BUFFER_MIN_SIZE = 128;
@@ -41,11 +47,28 @@ public class BinaryWriter {
 		this.written = temp;
 	}
 
+	/**
+	 * Writes the specified byte to this output stream. 
+	 * @param b the byte to be written.
+	 * @throws IOException
+	 */
 	public synchronized void write(int b) throws IOException {
 		out.write(b);
 		increaseCounter(1);
 	}
 
+	/**
+	 * Writes {@code length} bytes from the specified byte array starting at offset
+	 * {@code offset} to this output stream.<b/>
+	 * The {@code write} method of {@code DataOutputStream} calls the {@code write}
+	 * method of one argument on each byte to output.
+	 *
+	 * @param b The byte array to be written.
+	 * @param offset the start offset in the data.
+	 * @param length the number of bytes to write.
+	 * @throws IOException if an I/O error occurs.
+	 * @see OutputStream#write(byte[], int, int)
+	 */
 	public synchronized void write(byte[] b, int offset, int length) throws IOException {
 		out.write(b, offset, length);
 		increaseCounter(length);
@@ -66,6 +89,12 @@ public class BinaryWriter {
 		out.flush();
 	}
 
+	/**
+	 * Closes the stream and releases any system resources associated with it.<br/>
+	 * Once the stream has been closed, further write() invocations will throw an IOException.<br/>
+	 * Multiple calls to close are allowed and will have no effect.
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		if(closed) return;
 
@@ -118,19 +147,53 @@ public class BinaryWriter {
 		increaseCounter(1);
 	}
 
+	/**
+	 * Writes a {@code byte} to the underlying output stream as a 1-byte value.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by {@code 1}.
+
+	 * @param value A {@code byte} value to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeByte(byte value) throws IOException {
 		this.writeUnsignedByte(value);
 	}
 
+	/**
+	 * Writes a {@code UnsignedByte} to the underlying output stream. 
+	 * Because Java doesn't have unsigned byte, it is represented as an integer.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by {@code 1}.
+
+	 * @param value A {@code UnsignedByte} value to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeUnsignedByte(int value) throws IOException {
 		out.write(value);
 		increaseCounter(1);
 	}
 
+	/**
+	 * Writes a {@code short} to the underlying output stream as two bytes, high
+	 * byte first.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by
+	 * {@code 2}.
+	 * 
+	 * @param value A {@code short} value to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeShort(short value) throws IOException {
 		this.writeUnsignedShort(value);
 	}
-
+	
+	/**
+	 * Writes a {@code char} to the underlying output stream as a 2-byte value, high
+	 * byte first.
+	 * Because Java doesn't have unsigned short, it is represented as an integer.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by
+	 * {@code 2}.
+	 * 
+	 * @param value A {@code char} value to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeUnsignedShort(int value) throws IOException {
 		writeBuffer[0] = (byte)(value >>> 8);
 		writeBuffer[1] = (byte)(value >>> 0);
@@ -138,11 +201,29 @@ public class BinaryWriter {
 		out.write(writeBuffer, 0, 2);
 		increaseCounter(2);
 	}
-
+	
+	/**
+	 * Writes a {@code char} to the underlying output stream as a 2-byte value, high
+	 * byte first.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by
+	 * {@code 2}.
+	 * 
+	 * @param value A {@code char} value to be written.
+	 * @throws IOException if an I/O error occurs
+	 */
 	public void writeChar(char value) throws IOException {
 		this.writeUnsignedShort(value);
 	}
 
+	/**
+	 * Writes an {@code int} to the underlying output stream as four bytes, high
+	 * byte first.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by
+	 * {@code 4}.
+	 * 
+	 * @param value An {@code int} to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeInt(int value) throws IOException {
 		writeBuffer[0] = (byte)(value >>> 24);
         writeBuffer[1] = (byte)(value >>> 16);
@@ -152,7 +233,16 @@ public class BinaryWriter {
         out.write(writeBuffer, 0, 4);
         increaseCounter(4);
 	}
-
+	
+	/**
+	 * Writes a {@code long} to the underlying output stream as eight bytes, high
+	 * byte first.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by
+	 * {@code 8}.
+	 * 
+	 * @param value A {@code long} to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeLong(long value) throws IOException {
         writeBuffer[0] = (byte)(value >>> 56);
         writeBuffer[1] = (byte)(value >>> 48);
@@ -167,14 +257,40 @@ public class BinaryWriter {
         increaseCounter(8);
     }
 
+	/**
+	 * Writes a {@code float} to the underlying output stream as four bytes, high
+	 * byte first.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by
+	 * {@code 4}.
+	 * 
+	 * @param value A {@code float} to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeFloat(float value) throws IOException {
 		this.writeInt(Float.floatToIntBits(value));
 	}
-
+	
+	/**
+	 * Writes a {@code double} to the underlying output stream as eight bytes, high
+	 * byte first.<br/>
+	 * If no exception is thrown, the counter {@code written} is incremented by
+	 * {@code 8}.
+	 * 
+	 * @param value A {@code double} to be written.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeDouble(double value) throws IOException {
 		this.writeLong(Double.doubleToLongBits(value));
 	}
 
+	/**
+	 * Writes a string to the underlying output stream using the specified charset.
+	 * The length of the string is written, followed by the string itself.
+	 * 
+	 * @param str     A {@code String} value to be written.
+	 * @param charset A {@code Charset} value to be used.
+	 * @throws IOException if an I/O error occurs.
+	 */
 	public void writeUTF(String str, Charset charset) throws IOException {
 		int bytelen = 0;
 		switch(charset) {
@@ -255,6 +371,14 @@ public class BinaryWriter {
 		return strByteLen+4;
 	}
 
+	/**
+	 * Writes a {@code DateTime} to the underlying output stream. The control byte
+	 * is written first, followed by the fields of the {@code DateTime} object.
+	 * 
+	 * @param dt A {@code DateTime} value to be written.
+	 * @throws IOException if an I/O error occurs
+	 * @see DateTime()
+	 */
 	public void writeDatetime(DateTime dt) throws IOException {
 		final byte[] byteBuffer;
 		final int dtlen = 1 /*control byte*/ + DateTime.FIELDS_NUMBER /*number of fields*/;
