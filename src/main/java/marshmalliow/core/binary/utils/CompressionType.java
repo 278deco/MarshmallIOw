6a1f348dfd56d06432319afbee5d987f8c620c26
278deco <278deco.officiel@gmail.com>
2025-09-03 19:30:34 +0200

    feat(MOBF/Compression): add new AUTOMATIC type to determine compression automatically

--- src/main/java/marshmalliow/core/binary/utils/CompressionType.java
+++ src/main/java/marshmalliow/core/binary/utils/CompressionType.java
@@ -6,16 +6,24 @@ public enum CompressionType {
 	 * GZIP compression
 	 * @see <a href="https://tools.ietf.org/html/rfc1952">RFC 1952</a>
 	 */
-	GZIP(new byte[] {0x1F, (byte)0x8B}),
+	GZIP(new byte[] {0x1F, (byte)0x8B}, true),
+	
+	/**
+	 * Automatic detection of compression type based on the signature bytes
+	 * @see CompressionType#getSignature()
+	 */
+	AUTOMATIC(new byte[] {}, false),
 	
 	/**
 	 * No compression
 	 */
-	NONE(new byte[] {0x00});
+	NONE(new byte[] {0x00}, false);
 	
 	private byte[] signature;
-	private CompressionType(byte[] signature) {
+	private boolean realAlgorithm;
+	private CompressionType(byte[] signature, boolean realAlgorithm) {
 		this.signature = signature;
+		this.realAlgorithm = realAlgorithm;
 	}
 	
 	public byte[] getSignature() {
@@ -24,6 +32,10 @@ public enum CompressionType {
 	
 	public int getSignatureLength() {
 		return signature.length;
+	}
+	
+	public boolean isRealAlgorithm() {
+		return realAlgorithm;
 	}
 	
 }
