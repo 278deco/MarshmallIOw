3a8c364ee974064f150fb4396b0662e12bfbdf0a
278deco <278deco.officiel@gmail.com>
2025-09-22 22:18:40 +0200

    chore(test/dotenv): changed test directory for dotenv files

--- core/src/test/java/marshmallIOw/core/io/DotenvParserTest.java
+++ core/src/test/java/marshmallIOw/core/io/DotenvParserTest.java
@@ -1,4 +1,4 @@
-package marshmallIOw.core.file.dotenv;
+package marshmallIOw.core.io;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
